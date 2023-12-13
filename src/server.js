d2894fabee90bd5c8a35c502cf6931c876ae734b
DucBac99 <ducbac1411.mta@gmail.com>
2023-12-13 15:27:04 +0700

    sql connect

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine');
 const webRoute = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 const app = express()   //app express
 const port = process.env.PORT || 8888   // khai báo port
@@ -14,20 +14,13 @@ configViewEngine(app)
 //khai baso route
 app.use('/', webRoute)
 
-//test connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit'
-});
+
 
 connection.query(
     'select * from Users u',
     function(err, results, fields) {
       console.log(">>>>results: ",results); // results contains rows returned by server
-      console.log(">>>>fields: ",fields); // fields contains extra meta data about results, if available
+      // console.log(">>>>fields: ",fields); // fields contains extra meta data about results, if available
     }
   );
 
